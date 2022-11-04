32b12118b04ba20788a68b24b438388b4ae63a70
namjihyeon1228@gmail.com <namjihyeon1228@gmail.com>
2022-11-04 15:24:39 +0900

    코드 수정

--- PlayerInput.cs
+++ PlayerInput.cs
@@ -4,28 +4,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
+    private Input inputActions;
+
     public event Action<Vector2> MoveEvent;
     public event Action<bool> AttackEvent;
-    public event Action<bool> AttackCanceledEvent;
+    //public event Action<bool> AttackCanceledEvent;
 
-    private float moveValue;
+    void Awake()
+    {
+        inputActions = new Input();
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         MoveEvent.Invoke(context.ReadValue<Vector2>());
     }
 
-    public void OnAttack(InputAction.CallbackContext context)
+    private void Update()
     {
-        if (context.performed)
-        {
-            Debug.Log("몇번 호출?");
-            AttackEvent.Invoke(true);
-        }
-
-        if (context.canceled)
-        {
-            AttackCanceledEvent.Invoke(false);
-        }
+        inputActions.Player.Attack.started += _ => AttackEvent.Invoke(true);
+        inputActions.Player.Attack.canceled += _ => AttackEvent.Invoke(false);
     }
+
+    //public void OnAttack(InputAction.CallbackContext context)
+    //{
+    //    if (context.performed)
+    //    {
+    //        Debug.Log("몇번 호출?");
+    //        AttackEvent.Invoke(true);
+    //    }
+
+    //    if (context.canceled)
+    //    {
+    //        AttackCanceledEvent.Invoke(false);
+    //    }
+    //}
 }
